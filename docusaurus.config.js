90c34e02fb9aad16d3dcf1691412e3d8d06ff46b
Rophy Tsai <rophy@users.noreply.github.com>
2026-04-03 07:30:36 +0000

    build: configure GitHub Pages deployment

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,8 +2,9 @@ module.exports = {
   title: 'Testing Library',
   tagline:
     'Simple and complete testing utilities that encourage good testing practices',
-  url: 'https://testing-library.com',
-  baseUrl: '/',
+  url: 'https://rophy.github.io',
+  baseUrl: '/testing-library-docs/',
+  organizationName: 'rophy',
   projectName: 'testing-library-docs',
   scripts: [
     'https://buttons.github.io/buttons.js',
